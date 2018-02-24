e82b949df49f65bb28655e67facfee51b2f346d0
Slavisa Karalic <slavisa.karalic@gmail.com>
2018-02-24 10:43:36 +0100

    Preparing for packaging
    small changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # djangocms-workflows
 Moderation workflows for Django-CMS
+
+Forked from: https://github.com/Blueshoe/djangocms-workflows
+
+Alpha Software, use at your own risk.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import uuid
-import workflows
+from setuptools import find_packages, setup
 
 from pip.req import parse_requirements
 
@@ -16,27 +16,31 @@ def read(fname):
 
 requirements = [str(ir.req) for ir in parse_requirements('requirements.txt', session=uuid.uuid1())]
 
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
 setup(
-    name=workflows.__name__,
-    version=workflows.__version__,
-    description='Workflow system for Django-CMS users which aims for ease of use.',
-    long_description=read('README.md'),
+    name='djangocms-workflows',
+    version='0.2a1',
+    description='Workflow system for Django-CMS.',
+    long_description=README,
     license=read('LICENSE'),
-    author='Blueshoe',
-    author_email='TODO@TODO.com',
-    url='https://github.com/Blueshoe/djangocms-workflows',
-    packages=['workflows'],
+    author='karalics',
+    author_email='slavisa.karalic@gmail.com',
+    url='https://github.com/karalics/djangocms-workflows.git',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    keywords=['django', 'Django CMS', 'workflow', 'bootstrap', 'website', 'CMS', 'Blueshoe'],
+    keywords=['django', 'Django CMS', 'workflow', 'bootstrap', 'website', 'CMS'],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 -Alpha',
         'Framework :: Django',
         'Environment :: Web Environment',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
